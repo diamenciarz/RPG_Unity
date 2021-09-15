4347dd829689509c1369de01d6135d946e919152
Staś Ostaszewski <stasostaszewski@gmail.com>
2021-09-15 22:02:33 +0200

    Not

--- Assets/Scripts/PlayerNotificationController.cs
+++ Assets/Scripts/PlayerNotificationController.cs
@@ -1,11 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using 
 
 public class PlayerNotificationController : MonoBehaviour
 {
-    public Text
     // Start is called before the first frame update
     private void Start()
     {
