d4e206b639208303344f4ddaead90494d0d8441e
Staś Ostaszewski <stasostaszewski@gmail.com>
2021-09-16 15:56:22 +0200

    Inventory Slots

--- Assets/Scripts/Inventory/InventoryObject.cs
+++ Assets/Scripts/Inventory/InventoryObject.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryObject : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")]
 
-    // Update is called once per frame
-    void Update()
+public class InventoryObject : ScriptableObject
+{ 
+    public List<InventorySlot> inventorySlotContainer = new List<InventorySlot>();
+
+    public void AddItemToInventory(ItemObject inputItem, int itemAmount)
     {
-        
+        bool hasItem = false;
+        for (int i = 0; i < inventorySlotContainer.Count; i++)
+        {
+            if (inventorySlotContainer[i].item == inputItem)
+            {
+                inventorySlotContainer[i].AddAmount(itemAmount);
+                hasItem = true;
+                break;
+            }
+        }
+        if (hasItem == false)
+        {
+            inventorySlotContainer.Add(new InventorySlot(inputItem,itemAmount));
+        }
+    }
+}
+[System.Serializable]
+public class InventorySlot
+{
+    public ItemObject item;
+    public int amount;
+
+    public InventorySlot(ItemObject inputItem,int itemAmount)
+    {
+        item = inputItem;
+        amount = itemAmount;
+    }
+    public void AddAmount(int value)
+    {
+        amount += value;
     }
 }
