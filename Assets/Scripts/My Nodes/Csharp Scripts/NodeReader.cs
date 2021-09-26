e484bd200c6aa54e83301ef32cedd8f51a96d82c
Staś Ostaszewski <stasostaszewski@gmail.com>
2021-09-26 19:54:05 +0200

    Node Setup from tutorial

---
+++ Assets/Scripts/My Nodes/Csharp Scripts/NodeReader.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using XNode;
+
+public class NodeReader : MonoBehaviour
+{
+    public DialogueGraph dialogueGraph;
+    Coroutine reader;
+
+    public Text speaker;
+    public Text dialogue;
+
+    private void Start()
+    {
+        foreach (BaseNode bn in dialogueGraph.nodes)
+	    {
+            if (bn.GetString() == "Start")
+            {
+                dialogueGraph.current = bn;
+                break;
+            }
+	    }
+        reader = StartCoroutine(ReadNode());
+    }
+    IEnumerator ReadNode()
+    {
+        BaseNode bn = dialogueGraph.current;
+        string data = bn.GetString();
+        string[] stringArray = data.Split('/');
+        //Exception, when starting dialogue
+        if (stringArray[0] == "DialogueNode")
+        {
+            GoToNextNodeThroughOutput("Exit");
+
+        }
+        //
+        if (stringArray[0] == "DialogueNode")
+        {
+            speaker.text = stringArray[1];
+            dialogue.text = stringArray[2];
+            //Wait for one click, before going further with the dialogue
+            yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+            yield return new WaitUntil(() => Input.GetMouseButtonUp(0));
+
+            GoToNextNodeThroughOutput("Exit");
+        }
+    }
+    private void GoToNextNodeThroughOutput(string outputName)
+    {
+        if (reader != null)
+        {
+            StopCoroutine(reader);
+            reader = null;
+        }
+        foreach (NodePort np in dialogueGraph.current.Ports)
+        {
+            if (np.fieldName == outputName)
+            {
+                //Set 
+                dialogueGraph.current = np.Connection.node as BaseNode;
+                break;
+            }
+        }
+        reader = StartCoroutine(ReadNode());
+    }
+}
---
+++ Assets/Scripts/My Nodes/xNode Scripts/DialogueNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class DialogueNode : BaseNode {
+
+	[Input] public int entry;
+	[Output] public int exit;
+
+	public string speakerName;
+	public string dialogue;
+
+    public override string GetString()
+    {
+        return ("DialogueNode/" + speakerName + "/" + dialogue);
+    }
+}
